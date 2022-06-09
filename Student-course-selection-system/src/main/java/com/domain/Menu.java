123998ac46ba912f6cb338811ab8f0603685bc69
dullandmeanless <15889951171@163.com>
2022-06-09 14:01:50 +0800

    renthouse-code

--- Student-course-selection-system/src/main/java/com/domain/Menu.java
+++
@@ -1,7 +0,0 @@
-package com.domain;
-
-import com.baomidou.mybatisplus.annotation.TableName;
-
-@TableName(value = "courese_selection.menu")
-public class Menu {
-}
--- Student-course-selection-system/src/main/java/com/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import com.domain.Result;
-import com.domain.User;
-
-public interface LoginService {
-    Result login(User user);
-    Result logout();
-}
